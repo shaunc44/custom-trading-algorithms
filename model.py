049a9b8f7df27e82fdc36d51b58f423e10eaaebc
Shaun <seyyes2000@hotmail.com>
2017-03-27 18:09:51 -0400

    updates

--- model.py
+++ model.py
@@ -1,9 +1,15 @@
+import flask
 import mysql
+import wrapper
 #use pandas in here, not createdb
 #use data from Quandl, Xignite
 
 
-class Model:
+conn = mysql.connect("stock_algo.db")
+cursor = conn.cursor
+
+
+class Model():
 
 	def initialize(context):
 		#bring in init params

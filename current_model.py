bb856a0e3f317b75401914f8cfaf8d7b161c69df
Shaun <seyyes2000@hotmail.com>
2017-05-01 18:21:07 -0400

    work on current data

--- current_model.py
+++ current_model.py
@@ -27,59 +27,74 @@ class AddCurrentDataToPortfolio:
 	def add_current_data(self): #how to change startdate to curr date?
 		# sell_list = []
 		c.execute('''
-			UPDATE portfolio
-				INNER JOIN price 
-				ON portfolio.ticker_id = price.ticker_id
-			SET portfolio.current_price = %s,
-				portfolio.sell_price = %s
-			WHERE portfolio.ticker_id = %s AND portfolio.sell_price = 0;
-
-
-
 			SELECT portfolio.ticker_id, price.date, price.adj_close
 			FROM portfolio 
 			INNER JOIN price 
 			ON portfolio.ticker_id = price.ticker_id 
-			WHERE price.rsi > %s 
-			AND price.date = %s;
-			''', (self.rsi_sell, self.startdate_db)
+			WHERE price.date = %s;
+			''', (self.startdate_db)
 		)
-		stocks_to_sell = c.fetchall() 
-		print ("Possible Stocks to Sell = ", stocks_to_sell)
 
-		for row in stocks_to_sell:
-			symbol = row[0]
-			# Add sell_date and sell_price to portfolio
-			c.execute('''
-				UPDATE portfolio 
-				SET portfolio.sell_date = %s,
-					portfolio.sell_price = %s
-				WHERE portfolio.ticker_id = %s AND portfolio.sell_price = 0;
-				''', 
-				(row[1], row[2], row[0])
-			)
+		current = c.fetchall()
 
-			# Add sell_value and days_held to portfolio
+		for row in current:
+			print ("Current = ", row)
+
 			c.execute('''
-				UPDATE portfolio
-				SET portfolio.sell_value = ((portfolio.sell_price / portfolio.buy_price) * portfolio.buy_value),
-					portfolio.days_held = DATEDIFF(portfolio.sell_date, portfolio.buy_date)
-				WHERE portfolio.ticker_id = %s AND portfolio.sell_value = 0;
+				UPDATE IGNORE portfolio
+				SET portfolio.curr_price = %s
+				WHERE (portfolio.ticker_id = %s) 
+				AND (portfolio.sell_date = 0);
+				UPDATE IGNORE portfolio
+				SET portfolio.curr_value = ((portfolio.curr_price / portfolio.buy_price) * portfolio.buy_value)
+				WHERE portfolio.sell_date = 0;
 				''',
-				(row[0])
+				(row[2], row[0])
 			)
-
-			# Add gain_loss to portfolio
-			c.execute('''
-				UPDATE portfolio
-				SET portfolio.gain_loss = (portfolio.sell_value - portfolio.buy_value)
-				WHERE portfolio.ticker_id = %s AND portfolio.gain_loss = 0;
-				''',
-				(row[0])
-			)
-
 			conn.commit()
 		conn.close()
+
+
+
+
+
+
+		# stocks_to_sell = c.fetchall() 
+		# print ("Possible Stocks to Sell = ", stocks_to_sell)
+
+		# for row in stocks_to_sell:
+		# 	symbol = row[0]
+		# 	# Add sell_date and sell_price to portfolio
+		# 	c.execute('''
+		# 		UPDATE portfolio 
+		# 		SET portfolio.sell_date = %s,
+		# 			portfolio.sell_price = %s
+		# 		WHERE portfolio.ticker_id = %s AND portfolio.sell_price = 0;
+		# 		''', 
+		# 		(row[1], row[2], row[0])
+		# 	)
+
+		# 	# Add sell_value and days_held to portfolio
+		# 	c.execute('''
+		# 		UPDATE portfolio
+		# 		SET portfolio.sell_value = ((portfolio.sell_price / portfolio.buy_price) * portfolio.buy_value),
+		# 			portfolio.days_held = DATEDIFF(portfolio.sell_date, portfolio.buy_date)
+		# 		WHERE portfolio.ticker_id = %s AND portfolio.sell_value = 0;
+		# 		''',
+		# 		(row[0])
+		# 	)
+
+		# 	# Add gain_loss to portfolio
+		# 	c.execute('''
+		# 		UPDATE portfolio
+		# 		SET portfolio.gain_loss = (portfolio.sell_value - portfolio.buy_value)
+		# 		WHERE portfolio.ticker_id = %s AND portfolio.gain_loss = 0;
+		# # 		''',
+		# 		(row[0])
+		# 	)
+
+		# 	conn.commit()
+		# conn.close()
 
 
 
